c1271a6ba28195bb83bc8c52240318b8a75a684b
Sonia Lau <soniamacbookair@Sonias-MacBook-Air.local>
2022-03-01 23:43:29 -0800

    upload updated methods and locators

--- adshopcart_methods.py
+++ adshopcart_methods.py
@@ -11,6 +11,8 @@ from selenium.webdriver.chrome.service import Service
 driver = webdriver.Chrome('/Users/soniamacbookair/PycharmProjects/python_cctb/chromedriver')
 
 # Fixture method - to open web browser
+
+
 def setUp():
     # Make a full screen
     driver.maximize_window()
@@ -36,6 +38,8 @@ def setUp():
         driver.quit()
 
 # Fixture method - to close web browser
+
+
 def tearDown():
     if driver is not None:
         print(f'------------------------------------')
@@ -43,6 +47,118 @@ def tearDown():
         driver.close()
         driver.quit()
 
+def signUp():
+    driver.find_element(By.ID, 'menuUser').click()
+    sleep(3)
+    driver.find_element(By.LINK_TEXT, 'CREATE NEW ACCOUNT').click()
+    sleep(3)
+    driver.find_element(By.NAME, 'usernameRegisterPage').send_keys(locators.new_username)
+    sleep(0.25)
+    driver.find_element(By.NAME, 'emailRegisterPage').send_keys(locators.email)
+    sleep(0.25)
+    driver.find_element(By.NAME, 'passwordRegisterPage').send_keys(locators.new_password)
+    sleep(0.25)
+    driver.find_element(By.NAME,'confirm_passwordRegisterPage').send_keys(locators.new_password)
+    sleep(0.25)
+    driver.find_element(By.NAME,'first_nameRegisterPage').send_keys(locators.first_name)
+    sleep(0.25)
+    driver.find_element(By.NAME, 'last_nameRegisterPage').send_keys(locators.last_name)
+    sleep(0.25)
+    driver.find_element(By.NAME,'phone_numberRegisterPage').send_keys(locators.phone)
+    sleep(0.25)
+    Select(driver.find_element(By.NAME,'countryListboxRegisterPage')).select_by_visible_text('Canada')
+    sleep(0.25)
+    driver.find_element(By.NAME,'cityRegisterPage').send_keys(locators.city)
+    sleep(0.25)
+    driver.find_element(By.NAME, 'addressRegisterPage').send_keys(locators.address)
+    sleep(0.25)
+    driver.find_element(By.NAME,'state_/_province_/_regionRegisterPage').send_keys(locators.province)
+    sleep(0.25)
+    driver.find_element(By.NAME,'postal_codeRegisterPage').send_keys(locators.postal_code)
+    sleep(0.25)
+    driver.find_element(By.NAME,'allowOffersPromotion').click()
+    sleep(0.25)
+    driver.find_element(By.NAME,'i_agree').click()
+    sleep(0.25)
+    driver.find_element(By.ID,'register_btnundefined').click()
+    sleep(3)
+    driver.find_element(By.ID, 'menuUser').click()
+    sleep(3)
+    driver.find_element(By.XPATH, '//*[@id="loginMiniTitle"]/label[1]').click()
+    sleep(2)
+
+
+def check_my_account_display_full_name():
+    if driver.current_url == locators.my_account_url:
+        if driver.find_element(By.XPATH, f'//label[contains(.,"{locators.full_name}")]').is_displayed():
+            print(f'------------------------------------')
+            print(f'User with the name {locators.full_name} is displayed. Create account passed.')
+        else:
+            print('User name not displayed. Check your code.')
+
+
+def check_no_order():
+    driver.find_element(By.ID, 'menuUser').click()
+    sleep(3)
+    driver.find_element(By.XPATH, '//*[@id="loginMiniTitle"]/label[2]').click()
+    sleep(2)
+    if driver.find_element(By.XPATH, f'//label[contains(.," - No orders - ")]').is_displayed():
+        print(f'------------------------------------')
+        print(f'Confirmed No order is displayed')
+
+
+def sign_out():
+    driver.find_element(By.ID, 'menuUser').click()
+    sleep(3)
+    driver.find_element(By.XPATH, '//*[@id="loginMiniTitle"]/label[3]').click()
+    sleep(3)
+
+def log_in():
+    driver.find_element(By.ID, 'menuUser').click()
+    sleep(3)
+    driver.find_element(By.NAME, 'username').send_keys(locators.new_username)
+    sleep(2)
+    driver.find_element(By.NAME, 'password').send_keys(locators.new_password)
+    sleep(2)
+    driver.find_element(By.ID,'sign_in_btnundefined').click()
+    sleep(2)
+
+
+def delete_account():
+    driver.find_element(By.ID, 'menuUser').click()
+    sleep(3)
+    driver.find_element(By.XPATH, '//*[@id="loginMiniTitle"]/label[1]').click()
+    sleep(2)
+    driver.execute_script("window.scrollTo(1000,1000 )")
+    driver.find_element(By.XPATH, '//*[@id="myAccountContainer"]/div[6]/button/div').click()
+    sleep(5)
+    driver.find_element(By.XPATH, '//div[text()="yes"]').click()
+    sleep(5)
+
+
+def check_re_login():
+    driver.find_element(By.ID, 'menuUser').click()
+    sleep(3)
+    driver.find_element(By.NAME, 'username').send_keys(locators.new_username)
+    sleep(2)
+    driver.find_element(By.NAME, 'password').send_keys(locators.new_password)
+    sleep(2)
+    driver.find_element(By.ID, 'sign_in_btnundefined').click()
+    sleep(5)
+    if driver.find_element(By.ID,'signInResultMessage'):
+        print(f'------------------------------------')
+        print(f'Re-login failed. Test passed. ')
+    else:
+        print('Check your code for error.')
+
 
 setUp()
+signUp()
+check_my_account_display_full_name()
+check_no_order()
+sign_out()
+log_in()
+delete_account()
+check_re_login()
 tearDown()
+
